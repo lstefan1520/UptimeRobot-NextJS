942994e0f18200c4f80487e5b15698290479addb
Mark David <44349634+markd69@users.noreply.github.com>
2020-07-27 22:04:06 -0400

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-let uptimeRobotToken = "ur899712-6058a7b7cacc84540a172c95";
+let uptimeRobotToken = "ur981962-2554a26a15de4e02bb22510d";
 // let uptimeRobotToken = "ur899712-6058a7b7cacc84540a172c5";
 
 export { uptimeRobotToken };
